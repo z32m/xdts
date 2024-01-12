5df6f6c4b24a5b2968967de0e7459e59b287cc16
Serhii Kuzmychov <ku3cmich@gmail.com>
2024-01-12 11:07:45 +0200

    define device

--- xdts.h
+++ xdts.h
@@ -1,6 +1,7 @@
 #ifndef __xdts_h__
 #define __xdts_h__
 
-#define L(x) DT_NODELABEL(x)
+#define L(_node_label) DT_NODELABEL(_node_label)
+#define DEFINE_DEVICE(_node_label) const struct device *_node_label = DEVICE_DT_GET(L(_node_label))
 
 #endif
